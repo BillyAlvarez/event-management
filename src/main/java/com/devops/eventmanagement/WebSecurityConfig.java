d92107db1282b81a0e28e9a297605bafb13abca8
billy-alvarez <billyjag1@gmail.com>
2024-07-21 01:19:19 -0400

    implementacion seguridad de url segun rol

--- src/main/java/com/devops/eventmanagement/WebSecurityConfig.java
+++ src/main/java/com/devops/eventmanagement/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
+@EnableGlobalAuthentication
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
@@ -45,11 +47,11 @@ public class WebSecurityConfig {
                         .requestMatchers("/css/**").permitAll()
                         .requestMatchers("/js/**").permitAll()
                         .requestMatchers("/images/**").permitAll()
-                        .requestMatchers("/views/clientes/").hasAnyRole("USER")
-                        .requestMatchers("/views/clientes/create").hasAnyRole("ADMIN")
-                        .requestMatchers("/views/clientes/save").hasAnyRole("ADMIN")
-                        .requestMatchers("/views/clientes/edit/**").hasAnyRole("ADMIN")
-                        .requestMatchers("/views/clientes/delete/**").hasAnyRole("ADMIN")
+//                        .requestMatchers("/views/clientes/").hasAnyRole("USER")
+//                        .requestMatchers("/views/clientes/create").hasAnyRole("ADMIN")
+//                        .requestMatchers("/views/clientes/save").hasAnyRole("ADMIN")
+//                        .requestMatchers("/views/clientes/edit/**").hasAnyRole("ADMIN")
+//                        .requestMatchers("/views/clientes/delete/**").hasAnyRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .formLogin(formLogin -> formLogin
